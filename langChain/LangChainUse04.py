52f5aaee989cdf48f516aa1fe093342641a81698
charonry <1178807730@qq.com>
2026-03-24 16:45:17 +0800

    # LangChain-通用提示词模板（zero-shot）

---
+++ langChain/LangChainUse04.py
@@ -0,0 +1,22 @@
+"""
+langchain的通用提示词模板
+zero-shot：可以基于PromptTemplate直接完成
+"""
+from langchain_core.prompts import PromptTemplate
+from langchain_community.llms.tongyi import Tongyi
+
+model = Tongyi(model="qwen-max")
+# 通用提示词模板
+prompt_template = PromptTemplate.from_template("你叫{name}，年龄{age},你觉得你会是什么样子个性")
+
+# 方式一：模版注入
+"""
+# 调用.format方法注入信息
+prompt_text = prompt_template.format(name="prompt",age="5")
+res = model.invoke(prompt_text)
+"""
+
+# 方式二: chain执行链条
+chain = prompt_template | model
+res = chain.invoke({"name":"prompt","age":"5"})
+print(res)
