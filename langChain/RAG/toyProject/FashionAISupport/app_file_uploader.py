d1100e27929525adb991a5b69ae7ca60332c26f1
charonry <1178807730@qq.com>
2026-03-27 14:49:04 +0800

    # RAG-页面导入txt知识入向量库

--- langChain/RAG/toyProject/FashionAISupport/app_file_uploader.py
+++ langChain/RAG/toyProject/FashionAISupport/app_file_uploader.py
@@ -1,9 +1,12 @@
 """
 文件上传web服务
-运行 streamlit run .\langChain\RAG\toyProject\FashionAISupport\app_file_uploader.py
+运行 python -m streamlit run .\langChain\RAG\toyProject\FashionAISupport\app_file_uploader.py
+
+Streamlit：当WEB页面元素发生变化，则代码重新执行一遍
 """
 import time
 import streamlit as st
+from langChain.RAG.toyProject.FashionAISupport.knowledge_base import KnowledgeBase
 
 
 st.title("知识库更新服务")
@@ -15,6 +18,9 @@ uplader_file = st.file_uploader(
     accept_multiple_files=False
 )
 
+if "service" not in st.session_state:
+    st.session_state["service"] = KnowledgeBase()
+
 if uplader_file is not None:
     file_name = uplader_file.name
     file_type = uplader_file.type
@@ -24,4 +30,8 @@ if uplader_file is not None:
     st.write(f"格式：{file_type} | 大小：{file_size:.2f}KB")
 
     text = uplader_file.getvalue().decode("utf-8")
-    st.write(text)
+
+    with st.spinner("载入知识库中。。。"):
+        time.sleep(1)
+        result = st.session_state["service"].upload_by_str(text,file_name)
+        st.write(result)
